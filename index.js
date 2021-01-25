9cb022d71f564b2c43c77d363fd58689ddff6e37
Diego Laya <diegolaya01@hotmail.com>
2021-01-24 23:28:29 -0500

    polishing

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const emoji = require("emoji-dictionary");
 const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
-  multiplier: 0.8
+  multiplier: 1
 
 });
